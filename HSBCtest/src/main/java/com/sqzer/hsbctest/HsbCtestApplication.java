86c8254482b9b445840fcc9d737ad493ad2db50d
songqinzheng <157905285@qq.com>
2025-05-29 18:58:59 +0800

    FE202505291 创建项目

---
+++ HSBCtest/src/main/java/com/sqzer/hsbctest/HsbCtestApplication.java
@@ -0,0 +1,13 @@
+package com.sqzer.hsbctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbCtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsbCtestApplication.class, args);
+	}
+
+}
